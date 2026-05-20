3072bb12d6b16972ec435139d1e01e6f86fd5601
Bryam de Jesús Aguilar López <aguilarlopezbryam013@gmail.com>
2026-05-19 18:22:08 -0600

    correcion de folios

--- src/components/Assignment.jsx
+++ src/components/Assignment.jsx
@@ -1,14 +1,14 @@
 const processData = [
+  { code: 'F0315', name: 'ANGEL MIGUEL CASTILLA SANCHEZ' },
+  { code: 'F0312', name: 'NEHEMIAS GOMEZ G' },
+  { code: 'F0311', name: 'ESTEBAN URIEL GORDILLO C' },
   { code: 'F0306', name: 'FELIPE HERNÁNDEZ' },
-  { code: 'F0303', name: 'NEHEMIAS GOMEZ G' },
   { code: 'F0301', name: 'DIEGO AGUILAR J' },
-  { code: 'F0298', name: 'ESTEBAN URIEL GORDILLO C' },
-  { code: 'F0295', name: 'ANGEL MIGUEL CASTILLA SANCHEZ' },
   { code: 'F0287', name: 'MARIANA DE JESÚS PÉREZ N' },
 ]
 
 const awardedData = [
-  { code: 'F0300', name: 'EULALIO CHAGA C' },
+  { code: 'F0303', name: 'EULALIO CHAGA C' },
   { code: 'F0299', name: 'JAVIER CRUZ C' },
   { code: 'F0297', name: 'FRANCISCO ADAHYR ROMERO S' },
   { code: 'F0296', name: 'JOSÉ LUIS VÁSQUEZ G' },
