a52ec8cf09273565b1af636442aaacdc2f43c820
Schwi013 <aguilarlopezbryam013@gmail.com>
2026-08-21 09:48:18 -0600

    AGOSTO

--- src/components/Assignment.jsx
+++ src/components/Assignment.jsx
@@ -47,7 +47,7 @@ export default function Assignment() {
             </div>
           </div>
           <div className="board">
-            <div className="board-header">ADJUDICADOS DE JULIO</div>
+            <div className="board-header">ADJUDICADOS DE AGOSTO</div>
             <div className="board-body">
               {awardedData.map((row) => (
                 <div key={row.code} className="board-row">
