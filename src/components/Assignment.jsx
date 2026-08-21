28de8a9bf342259ce4253bbd1bd2168d3a6d8fcb
Schwi013 <aguilarlopezbryam013@gmail.com>
2026-08-21 09:44:48 -0600

    adjudicados agosto

--- src/components/Assignment.jsx
+++ src/components/Assignment.jsx
@@ -1,19 +1,20 @@
 const processData = [
-    { code: 'F0306', name: 'FELIPE P HERNANDEZ' },
-      { code: 'F0310', name: 'LEONARDO A. MAR YAÑEZ' },
-        { code: 'F0313', name: 'ALEJANDRA LEAL G.' },
-          { code: 'F0327', name: 'EMANUEL OMAR AGUILAR V.' },
-            { code: 'F0329', name: 'PEDRO MARTIN C.' },
-              { code: 'F0332', name: 'MIGUEL REYES A.' },
-                { code: 'F0333', name: 'ALEJANDRO ALBERTO S.' },
-                  { code: 'F0335', name: 'JULIANA HERNANDEZ U.' },
-                    { code: 'F0338', name: 'GUILLERMO LOPEZ R.' },
-                      { code: 'F0339', name: 'JOSE FELIX S.' },
-                        { code: 'F0340', name: 'DAVID NIEVES G.' },
-                          { code: 'F0341', name: 'SANDRA GUADALUPE C.' },
-                            { code: 'F0342', name: 'JOSE RODOLFO R.' }
-                              
-                              
+  { code: 'F0306', name: 'FELIPE P HERNANDEZ' },
+  { code: 'F0310', name: 'LEONARDO A. MAR YAÑEZ' },
+  { code: 'F0313', name: 'ALEJANDRA LEAL G.' },
+  { code: 'F0327', name: 'EMANUEL OMAR AGUILAR V.' },
+  { code: 'F0329', name: 'PEDRO MARTIN C.' },
+  { code: 'F0332', name: 'MIGUEL REYES A.' },
+  { code: 'F0333', name: 'ALEJANDRO ALBERTO S.' },
+  { code: 'F0335', name: 'JULIANA HERNANDEZ U.' },
+  { code: 'F0338', name: 'GUILLERMO LOPEZ R.' },
+  { code: 'F0339', name: 'JOSE FELIX S.' },
+  { code: 'F0340', name: 'DAVID NIEVES G.' },
+  { code: 'F0341', name: 'SANDRA GUADALUPE C.' },
+  { code: 'F0343', name: 'NOHEMI HERNANDEZ G' },
+  { code: 'F0344', name: 'GUSTAVO ROSAS C' },
+  { code: 'F0345', name: 'CARMEN DE JESUS M.' },
+  { code: 'F0346', name: 'JOSE MANUEL N.' }                          
 ]
 
 const awardedData = [
@@ -21,7 +22,8 @@ const awardedData = [
   { code: 'F0312', name: 'NEHEMIAS GOMEZ G' },
   { code: 'F0301', name: 'DIEGO AGUILAR J' },
   { code: 'F0328', name: 'JUSTO Z. BAEZ' },
-  { code: 'F0330', name: 'HECTOR MUÑOZ O.' }
+  { code: 'F0330', name: 'HECTOR MUÑOZ O.' },
+  { code: 'F0342', name: 'JOSE RODOLFO R.' }
 ]
 
 export default function Assignment() {
