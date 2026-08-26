e53d168e1c89984d4f86108cbb174aa4e4ac6630
Schwi013 <aguilarlopezbryam013@gmail.com>
2026-08-26 10:48:50 -0600

    Actualización de clientes

--- src/components/Assignment.jsx
+++ src/components/Assignment.jsx
@@ -3,15 +3,9 @@ const processData = [
   { code: 'F0310', name: 'LEONARDO A. MAR YAÑEZ' },
   { code: 'F0313', name: 'ALEJANDRA LEAL G.' },
   { code: 'F0327', name: 'EMANUEL OMAR AGUILAR V.' },
-  { code: 'F0329', name: 'PEDRO MARTIN C.' },
-  { code: 'F0332', name: 'MIGUEL REYES A.' },
   { code: 'F0333', name: 'ALEJANDRO ALBERTO S.' },
-  { code: 'F0335', name: 'JULIANA HERNANDEZ U.' },
-  { code: 'F0338', name: 'GUILLERMO LOPEZ R.' },
   { code: 'F0339', name: 'JOSE FELIX S.' },
   { code: 'F0340', name: 'DAVID NIEVES G.' },
-  { code: 'F0341', name: 'SANDRA GUADALUPE C.' },
-  { code: 'F0343', name: 'NOHEMI HERNANDEZ G' },
   { code: 'F0344', name: 'GUSTAVO ROSAS C' },
   { code: 'F0345', name: 'CARMEN DE JESUS M.' },
   { code: 'F0346', name: 'JOSE MANUEL N.' }                          
@@ -23,6 +17,7 @@ const awardedData = [
   { code: 'F0301', name: 'DIEGO AGUILAR J' },
   { code: 'F0328', name: 'JUSTO Z. BAEZ' },
   { code: 'F0330', name: 'HECTOR MUÑOZ O.' },
+  { code: 'F0338', name: 'GUILLERMO LOPEZ R.' },
   { code: 'F0342', name: 'JOSE RODOLFO R.' }
 ]
 
